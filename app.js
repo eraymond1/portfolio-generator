7d989025262cdef2f63954fca79a22174779ea15
eraymond1 <ericraymond@rocketmail.com>
2022-05-19 20:09:37 -0400

    added validation to questions

--- app.js
+++ app.js
@@ -18,13 +18,29 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name?(Required)',
+      validate:  nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      }
     },
 
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your Github Username'
+      message: 'Enter your Github Username (Required)',
+      validate: githubInput => {
+        if (githubInput){
+          return true;
+        } else {
+          console.log("Please enter your gitHub username!");
+          return false;
+        }
+      }
     },
 
     {
@@ -58,7 +74,29 @@ if(!portfolioData.projects){
     {
       type: 'input',
       name: 'name',
-      message: 'Waht is the name of your project?'
+      message: 'Waht is the name of your project? (Required)',
+      validate: projectNameInput => {
+        if (projectNameInput){
+          return true;
+        } else {
+          console.log("Enter gitHub project name!");
+          return false;
+        }
+      }
+    },
+
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)',
+      validate: projectDescription => {
+        if (projectDescription){
+          return true;
+        } else {
+          console.log("Provide a description of project!");
+          return false;
+        }
+      }
     },
 
     {
@@ -70,7 +108,15 @@ if(!portfolioData.projects){
     {
       type: 'input',
       name:  'link',
-      message: 'Enter the GitHub link to your project. (Required)'
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: gitHubLink => {
+        if (gitHubLink) {
+          return true;
+        } else {
+          console.log("GitHub Link must be entered!");
+          return false;
+        }
+      }
     },
 
     {
